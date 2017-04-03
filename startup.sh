6c9e6c1b59452cae9b68b001355980b1a4c4391f
Simon Harvey <Simon.Harvey@moneysupermarket.com>
2017-04-03 15:07:22 +0100

    updated startup

--- startup.sh
+++ startup.sh
@@ -19,6 +19,11 @@ sudo git clone https://github.com/simon71/centos7.git
 # copy to home folder
 cp centos7/.vimrc ~/.vimrc
 
+cp centos7/autoload ~/.vim/autoload
+
 # Install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim --insecure
+
+# install nerdtree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
