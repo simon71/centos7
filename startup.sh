351ae8cfbc88c8233063aab62b59a3d06a49a612
Simon Harvey <Simon.Harvey@moneysupermarket.com>
2017-07-12 15:48:26 +0100

    added create new user

--- startup.sh
+++ startup.sh
@@ -5,29 +5,38 @@ sudo yum -y update
 sudo yum -y upgrade
 sudo yum -y clean all
 
-check=$(yum list installed | grep vim)
+pw=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
 
-if [ -z "$check" ]; then
-	echo "installing vim..."
-	sudo yum -y install vim
-else
-	echo "vim already installed"
-fi
-
-# add me as user
+echo "creating user account simon71"
 /usr/sbin/useradd simon71 -g vagrant -G wheel
-echo
-
-# copy to home folder
-cp centos7/.vimrc ~/.vimrc
-
-cp centos7/autoload ~/.vim/autoload
-
-# Install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim --insecure
-
-# install nerdtree
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-#update PS1 cant get it to work
-P1="\[\033[1;93m\] \\A \\u \\w \[\033[0;96m\]$\[\033[0;37m\]"
+echo "the password is $pw"
+echo "$pw"|passwd --stdin vagrant
+echo "simon71        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/vagrant
+chmod 0440 /etc/sudoers.d/vagrant
+#
+# check=$(yum list installed | grep vim)
+#
+# if [ -z "$check" ]; then
+# 	echo "installing vim..."
+# 	sudo yum -y install vim
+# else
+# 	echo "vim already installed"
+# fi
+#
+# # add me as user
+# /usr/sbin/useradd simon71 -g vagrant -G wheel
+# echo
+#
+# # copy to home folder
+# cp centos7/.vimrc ~/.vimrc
+#
+# cp centos7/autoload ~/.vim/autoload
+#
+# # Install pathogen
+# mkdir -p ~/.vim/autoload ~/.vim/bundle
+# curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim --insecure
+#
+# # install nerdtree
+# git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+# #update PS1 cant get it to work
+# P1="\[\033[1;93m\] \\A \\u \\w \[\033[0;96m\]$\[\033[0;37m\]"
